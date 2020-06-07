3a011e27814e5f5310a4ba54c28ffd0c9750e7eb
eric noh <sehyun.noh83@gmail.com>
2020-06-07 00:15:12 -0400

    add

---
+++ Javascript/codesignal/Arcade/The Core/34.countBlackCells.js
@@ -0,0 +1,8 @@
+function countBlackCells(n, m) {
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {}
+  }
+}
+
+console.log(countBlackCells(3, 4)); //6
--- Javascript/codesignal/CompanyChallenge/1.armyMarch.js
+++ Javascript/codesignal/CompanyChallenge/1.armyMarch.js
@@ -1,5 +1,6 @@
 function armyMarch(a, b, v1, v2) {
   let y = a[1] - ((a[1] - b[1]) / (a[0] - b[0])) * a[0];
+  // let y = 1.31899;
   let fDis = Math.sqrt(a[0] ** 2 + (y - a[1]) ** 2);
   let sDis = Math.sqrt(b[0] ** 2 + (b[1] - y) ** 2);
   return fDis / v1 + sDis / v2;
