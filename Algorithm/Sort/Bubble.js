6d04de0979f2703a53d7e275149ba144f080ad72
eric noh <sehyun.noh83@gmail.com>
2020-07-04 23:49:21 -0400

    add

---
+++ Algorithm/Sort/Bubble.js
@@ -0,0 +1,16 @@
+let arr = [3, 7, 2, 1, 4, 10, 8];
+
+function bubble(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubble(arr));
